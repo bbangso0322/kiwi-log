6c68c7a9ff2460fd1c28c57e356ce2487150a651
bbangso0322 <bbangso0322@gmail.com>
2024-03-06 00:37:35 +0900

    design card

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -31,12 +31,15 @@ class LightMode{
 	setMode(isDarkMode) {
 		const modeIcon = document.getElementById("light-mode");
 		if(isDarkMode == "false") {
-			document.documentElement.style.setProperty("--background-color", 'green');
+			document.documentElement.style.setProperty("--background-color", 'white');
+			document.documentElement.style.setProperty("--background-gray", '#F8FAFD');
+			document.documentElement.style.setProperty("--background-hover", '#C2E7FF');
 			modeIcon.classList.remove("fa-moon");
 			modeIcon.classList.add("fa-sun");
 		}
 		else {
 			document.documentElement.style.setProperty("--background-color", 'red');
+			document.documentElement.style.setProperty("--background-gray", 'blue');
 			modeIcon.classList.remove("fa-sun");
 			modeIcon.classList.add("fa-moon");
 		}
