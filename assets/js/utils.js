9cf0bdf0683e67e9557d9f9b9f828d93ed4f8715
bbangso0322 <bbangso0322@gmail.com>
2024-03-11 23:22:15 +0900

    design search bar

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -30,16 +30,23 @@ class LightMode{
 
 	setMode(isDarkMode) {
 		const modeIcon = document.getElementById("light-mode");
+		const styler = document.documentElement.style;
 		if(isDarkMode == "false") {
-			document.documentElement.style.setProperty("--background-color", 'white');
-			document.documentElement.style.setProperty("--background-gray", '#F8FAFD');
-			document.documentElement.style.setProperty("--background-hover", '#C2E7FF');
+			styler.setProperty("--background-color", '#FFFBF7');
+			styler.setProperty("--background-secondary", '#F8F3EB');
+			styler.setProperty("--background-hover", '#FFDAD4');
+			styler.setProperty("--font-color", '#3C2421');
+			styler.setProperty("--background-search-bar", '#F6E9DE');
+			styler.setProperty("--search-icon-hover", '#86705B');
 			modeIcon.classList.remove("fa-moon");
 			modeIcon.classList.add("fa-sun");
 		}
 		else {
-			document.documentElement.style.setProperty("--background-color", 'red');
-			document.documentElement.style.setProperty("--background-gray", 'blue');
+			styler.setProperty("--background-color", 'red');
+			styler.setProperty("--background-secondary", 'blue');
+			styler.setProperty("--background-hover", '#FFDAD4');
+			styler.setProperty("--font-color", 'white');
+			styler.setProperty("--background-search-bar", '#3C2421');
 			modeIcon.classList.remove("fa-sun");
 			modeIcon.classList.add("fa-moon");
 		}
